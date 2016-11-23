1055827b358eb5c0a59829a5d574e4523667e148
Ulises Bocchio <ulisesb@gmail.com>
2016-11-22 19:08:12 -0800

    Update JarResourceLoader.java

--- spring-boot-jar-resources/src/main/java/com/github/ulisesbocchio/jar/resources/JarResourceLoader.java
+++ spring-boot-jar-resources/src/main/java/com/github/ulisesbocchio/jar/resources/JarResourceLoader.java
@@ -1,5 +1,9 @@
 package com.github.ulisesbocchio.jar.resources;
 
+import com.google.common.io.Files;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.core.env.Environment;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
@@ -10,10 +14,12 @@ import java.util.Optional;
 /**
  * @author Ulises Bocchio
  */
+@Slf4j
 public class JarResourceLoader implements ResourceLoader {
 
     private ResourceLoader delegate = new DefaultResourceLoader();
     private String extractPath;
+    private String resolvedExtractPath;
     private Environment environment;
     private String propertyName;
 
@@ -32,14 +38,22 @@ public class JarResourceLoader implements ResourceLoader {
     @Override
     public Resource getResource(String location) {
         Resource resource = delegate.getResource(location);
-        String resolvedExtractPath = resolveExtractPath();
+        String resolvedExtractPath = getResolvedExtractPath();
         return new JarResource(resource, resolvedExtractPath);
     }
 
-    private String resolveExtractPath() {
-        return Optional.ofNullable(environment)
+    private String getResolvedExtractPath() {
+        if(resolvedExtractPath == null) {
+            resolvedExtractPath = Optional.ofNullable(environment)
                 .map(env -> environment.getProperty(propertyName))
                 .orElse(extractPath);
+            if (extractPath == null) {
+                extractPath = Files.createTempDir().getAbsolutePath();
+                log.debug("TEMP EXTRACT DIR CREATED {}", extractPath);
+            }
+            log.debug("Resolved JarResource extract path to: {}", resolvedExtractPath);
+        }
+        return resolvedExtractPath;
     }
 
     @Override
